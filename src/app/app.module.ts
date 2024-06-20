302436243d76917b1945423d52aeee9bbe0108ce
Arthur <114860794+Howl404@users.noreply.github.com>
2024-06-20 11:47:12 +0300

    feat: firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,8 +16,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'posthub-8849f',
+    appId: '1:661540929803:web:225fa552714a941ee6220e',
+    storageBucket: 'posthub-8849f.appspot.com',
+    apiKey: 'AIzaSyDaGbhCIJlEGqW-KK7jX-uMz2ALUsEFX-E',
+    authDomain: 'posthub-8849f.firebaseapp.com',
+    messagingSenderId: '661540929803',
+  },
+};
