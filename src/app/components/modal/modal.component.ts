9a35b005a2ec6037247578970dfc91fe3a4b5986
Arthur <114860794+Howl404@users.noreply.github.com>
2024-06-25 12:43:55 +0300

    feat: model component and service

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,48 @@
+import { Component, ViewEncapsulation, ElementRef, Input, OnInit, OnDestroy } from '@angular/core';
+import { ModalService } from 'src/app/components/modal/modal.service';
+
+@Component({
+  selector: 'app-modal[id]',
+  templateUrl: 'modal.component.html',
+  styleUrls: ['modal.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class ModalComponent implements OnInit, OnDestroy {
+  @Input() id!: string;
+
+  isOpen = false;
+
+  private element: HTMLElement = this.el.nativeElement;
+
+  constructor(
+    private modalService: ModalService,
+    private el: ElementRef,
+  ) {}
+
+  ngOnInit(): void {
+    this.modalService.add(this);
+    document.body.appendChild(this.element);
+  }
+
+  handleOverlayClick(event: MouseEvent): void {
+    const element = event.target as HTMLElement;
+    if (element.className === 'modal-overlay') {
+      this.close();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.modalService.remove(this);
+    this.element.remove();
+  }
+
+  open(): void {
+    document.body.classList.add('modal-open');
+    this.isOpen = true;
+  }
+
+  close(): void {
+    document.body.classList.remove('modal-open');
+    this.isOpen = false;
+  }
+}
---
+++ src/app/components/modal/modal.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { ModalComponent } from 'src/app/components/modal/modal.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalService {
+  private modals: ModalComponent[] = [];
+
+  add(modal: ModalComponent): void {
+    if (this.modals.find((x) => x.id === modal.id)) {
+      throw new Error('modal must have a unique id attribute');
+    }
+
+    this.modals.push(modal);
+  }
+
+  remove(modal: ModalComponent): void {
+    this.modals = this.modals.filter((x) => x === modal);
+  }
+
+  open(id: string): void {
+    const modal = this.modals.find((x) => x.id === id);
+
+    if (!modal) {
+      throw new Error(`modal '${id}' not found`);
+    }
+
+    modal.open();
+  }
+
+  close(id: string): void {
+    const modal = this.modals.find((x) => x.id === id);
+
+    if (!modal) {
+      throw new Error(`modal '${id}' not found`);
+    }
+
+    modal.close();
+  }
+}
