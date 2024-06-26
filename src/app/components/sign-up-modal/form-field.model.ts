9a2947326e624aa35a6df49c8a11e29d4727e964
Arthur <114860794+Howl404@users.noreply.github.com>
2024-06-26 11:33:31 +0300

    feat: form fields and form field model

---
+++ src/app/components/sign-up-modal/form-field.model.ts
@@ -0,0 +1,9 @@
+export interface FormField {
+  placeholder: string;
+  type: string;
+  name: string;
+  model: string;
+  required: boolean;
+  minLength: number | null;
+  validationMessages?: { [key: string]: string };
+}
---
+++ src/app/components/sign-up-modal/sign-up-fields.ts
@@ -0,0 +1,58 @@
+import { FormField } from 'src/app/components/sign-up-modal/form-field.model';
+
+export const signUpFields: FormField[] = [
+  {
+    placeholder: 'Name',
+    type: 'text',
+    name: 'name',
+    model: 'name',
+    required: true,
+    minLength: 3,
+    validationMessages: {
+      required: 'Name is required.',
+      minlength: 'Name must be at least 3 characters long.',
+    },
+  },
+  {
+    placeholder: 'Email',
+    type: 'email',
+    name: 'email',
+    model: 'email',
+    required: true,
+    minLength: 6,
+    validationMessages: {
+      required: 'Email is required.',
+      email: 'Email must be a valid email address.',
+      minlength: 'Email must be at least 6 characters long.',
+    },
+  },
+  {
+    placeholder: 'Password',
+    type: 'password',
+    name: 'password',
+    model: 'password',
+    required: true,
+    minLength: 6,
+    validationMessages: {
+      required: 'Password is required.',
+      minlength: 'Password must be at least 6 characters long.',
+    },
+  },
+  {
+    placeholder: 'Date of Birth',
+    type: 'date',
+    name: 'dob',
+    model: 'dob',
+    required: true,
+    minLength: null,
+    validationMessages: { required: 'Date of Birth is required.' },
+  },
+  {
+    placeholder: 'Email subscribe',
+    type: 'checkbox',
+    name: 'subscribe',
+    model: 'subscribed',
+    required: false,
+    minLength: null,
+  },
+];
